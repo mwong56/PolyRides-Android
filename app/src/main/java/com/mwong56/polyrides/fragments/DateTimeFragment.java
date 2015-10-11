a342ab278d90087510b22fdcb82f70e84a569d38
mwong56 <michael.wong110@gmail.com>
2015-10-11 01:06:58 -0700

    date time picker looks good

--- app/src/main/java/com/mwong56/polyrides/fragments/DateTimeFragment.java
+++ app/src/main/java/com/mwong56/polyrides/fragments/DateTimeFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.mwong56.polyrides.R;
 import com.mwong56.polyrides.models.Date;
@@ -14,12 +15,22 @@ import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
+import java.util.Calendar;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DateTimeFragment extends Fragment implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener{
+public class DateTimeFragment extends Fragment implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
+
+  @Bind(R.id.chosen_date)
+  TextView dateTextView;
+
+  @Bind(R.id.chosen_time)
+  TextView timeTextView;
 
   private DateTimeListener listener;
   private Time time;
@@ -42,8 +53,8 @@ public class DateTimeFragment extends Fragment implements TimePickerDialog.OnTim
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    View view =  inflater.inflate(R.layout.fragment_date_time, container, false);
-    ButterKnife.bind(view);
+    View view = inflater.inflate(R.layout.fragment_date_time, container, false);
+    ButterKnife.bind(this, view);
     return view;
   }
 
@@ -63,15 +74,40 @@ public class DateTimeFragment extends Fragment implements TimePickerDialog.OnTim
     super.onDetach();
   }
 
+  @OnClick(R.id.dateButton)
+  void onDateButtonClicked() {
+    Calendar now = Calendar.getInstance();
+    DatePickerDialog dpd = DatePickerDialog.newInstance(
+        this,
+        now.get(Calendar.YEAR),
+        now.get(Calendar.MONTH),
+        now.get(Calendar.DAY_OF_MONTH)
+    );
+    dpd.show(getActivity().getFragmentManager(), "Datepickerdialog");
+  }
+
+  @OnClick(R.id.timeButton)
+  void onTimeButtonClicked() {
+    Calendar now = Calendar.getInstance();
+    TimePickerDialog tpd = TimePickerDialog.newInstance(
+        this,
+        now.get(Calendar.HOUR_OF_DAY),
+        now.get(Calendar.MINUTE),
+        false
+    );
+    tpd.show(getActivity().getFragmentManager(), "Timepickerdialog");
+  }
+
   @Override
   public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
     this.date = new Date(year, monthOfYear, dayOfMonth);
-
+    dateTextView.setText((monthOfYear + 1) + "/" + dayOfMonth + "/" + year);
   }
 
   @Override
   public void onTimeSet(RadialPickerLayout radialPickerLayout, int hourOfDay, int minute) {
     this.time = new Time(hourOfDay, minute);
+    timeTextView.setText(hourOfDay + ":" + minute);
   }
 
 }
