172385ad7efd41ed4c39b2b8d1cb1a879256ff5e
mwong56 <michael.wong110@gmail.com>
2015-10-26 02:06:53 -0700

    Placeholder for setting title of action bar to username chat

--- app/src/main/java/com/mwong56/polyrides/activities/MessageActivity.java
+++ app/src/main/java/com/mwong56/polyrides/activities/MessageActivity.java
@@ -8,11 +8,14 @@ import android.text.TextUtils;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 
+import com.facebook.AccessToken;
 import com.mwong56.polyrides.R;
 import com.mwong56.polyrides.adapters.MessageListAdapter;
 import com.mwong56.polyrides.models.Message;
 import com.mwong56.polyrides.models.Messages;
 import com.mwong56.polyrides.models.User;
+import com.mwong56.polyrides.services.FacebookService;
+import com.mwong56.polyrides.services.FacebookServiceImpl;
 import com.mwong56.polyrides.services.PolyRidesService;
 import com.mwong56.polyrides.services.PolyRidesServiceImpl;
 import com.mwong56.polyrides.views.SendButton;
@@ -43,6 +46,7 @@ public class MessageActivity extends BaseRxActivity {
   SendButton sendButton;
 
   private final PolyRidesService polyRidesService = PolyRidesServiceImpl.get();
+  private final FacebookService facebookService = FacebookServiceImpl.get();
   private final List<Message> messageList = new ArrayList<>();
   private MessageListAdapter adapter;
   private Messages messages;
@@ -54,6 +58,8 @@ public class MessageActivity extends BaseRxActivity {
     ButterKnife.bind(this);
 
     setSupportActionBar(toolbar);
+//    facebookService.getUserName(AccessToken.getCurrentAccessToken(), this.messages.getUserId2())
+//        .subscribe(userName -> setTitle(userName), onError -> showToast(onError));
 
     LinearLayoutManager layoutManager = new LinearLayoutManager(getBaseContext());
     layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
